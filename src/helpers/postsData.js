ae239bcdb16495097ce16e9621096fbc321d32a4
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-08-05 08:35:56 +0530

    post added and made media responsive

---
+++ src/helpers/postsData.js
@@ -0,0 +1,25 @@
+export const dummyAvatar = [
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/776.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/491.jpg',
+  'https://avatars.githubusercontent.com/u/77087818',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1161.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/393.jpg',
+  'https://avatars.githubusercontent.com/u/68547098',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1013.jpg',
+  'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/133.jpg',
+  'https://avatars.githubusercontent.com/u/80411145',
+  'https://avatars.githubusercontent.com/u/12414547',
+];
+
+export const dummyImage = [
+  'https://picsum.photos/seed/OHz1by/640/480',
+  'https://loremflickr.com/640/480?lock=7712269345488896',
+  'https://loremflickr.com/640/480?lock=5070302396547072',
+  'https://loremflickr.com/640/480?lock=3874137867026432',
+  'https://loremflickr.com/640/480?lock=6065210087440384',
+  'https://loremflickr.com/640/480?lock=551570007654400',
+  'https://loremflickr.com/640/480?lock=2815910633013248',
+  'https://picsum.photos/seed/73UdC/640/480',
+  'https://picsum.photos/seed/OU2Gl5ue2/640/480',
+  'https://picsum.photos/seed/mpexBlv/640/480',
+];
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,3 +1,52 @@
-const Posts = () => <div>List your posts</div>;
+import { useEffect, useState } from 'react';
+import { dummyAvatar, dummyImage } from '../helpers/postsData';
+import '../styles/posts.css';
+
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const baseUrl = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    fetch(`${baseUrl}/posts`)
+      .then(res => res.json())
+      .then(data => {
+        setPosts(data.data.posts);
+      })
+      .catch(() => {});
+  }, []);
+
+  return (
+    <main className="p-3">
+      <div className="text-center fs-4">List of Posts</div>
+
+      <div>
+        <div className="container">
+          {posts.map((post, index) => (
+            <div className="cardContainer" key={post.id}>
+              <div>
+                <div className="imageContainer">
+                  <img src={`${dummyImage[index]}`} alt="Post" />
+                </div>
+                <div className="writeup">
+                  <p>{post.writeup}</p>
+                </div>
+                <div className="bottomPush">
+                  <div className="flexContainer">
+                    <div className="avatarContainer">
+                      <img src={`${dummyAvatar[index]}`} className="rounded-circle" alt="Profile" />
+                    </div>
+                    <p className="fullName">
+                      {post.firstName} {post.lastName}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default Posts;
